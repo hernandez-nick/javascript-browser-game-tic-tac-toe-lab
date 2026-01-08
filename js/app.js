635eca5992bc73d587939180420dc0550a441da0
hernandeznick-ga <hernandeznick1990@gmail.com>
2026-01-07 19:32:31 -0800

    fourth commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 /*-------------------------------- Constants --------------------------------*/
-const squareEls = document.querySelectorAll('.square');
+const squareEls = document.querySelectorAll('.sqr');
 
 const messageEl = document.getElementById('message');
 
-
-
-
-
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 /*---------------------------- Variables (state) ----------------------------*/
 
 let board;
@@ -37,7 +43,7 @@ function render() {
 }
 
 
-function updateBoard() {{
+function updateBoard() {
     board.forEach((square, index) => {
         squareEls[index].textContent = square;
     });
@@ -45,14 +51,14 @@ function updateBoard() {{
 
 
 function updateMessage() {
-    if (winner, tie) === false {
+    if (winner === false && tie === false) {
         messageEl.textContent = `It's ${turn}'s turn`;
     } else if (winner === false && tie === true) {
         messageEl.textContent = `It's a tie!`;
     } else {
         messageEl.textContent = `Congratulations, ${turn} wins!`;
     }
-}}
+}
 
 
 
