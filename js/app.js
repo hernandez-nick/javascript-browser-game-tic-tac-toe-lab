98dbb68d3d5cbb637560cb4189ac9a971fce41e9
hernandeznick-ga <hernandeznick1990@gmail.com>
2026-01-07 18:22:29 -0800

    fixed file name

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 /*-------------------------------- Constants --------------------------------*/
+const squareEls = document.querySelectorAll('.square');
+
+const messageEl = document.getElementById('message');
+
+
 
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let board;
+let turn;
+let winner;
+let tie;
 
 /*------------------------ Cached Element References ------------------------*/
 
